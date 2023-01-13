50329f30c92bbb63c6c5ce8f32abbfc0a99ee47b
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-01-13 20:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let count; 
-document.getElementById("evaluatedText").value ="";
-document.addEventListener("keyup",
-						  function(){
-							  count=
-								  document.getElementById("evaluatedText").value;
-							  document.querySelector("#lettercount").textCount=count.length;
-						  });//your code here
+//your code here
